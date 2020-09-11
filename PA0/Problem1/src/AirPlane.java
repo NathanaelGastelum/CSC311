9dcb2f4294d3db1d225ea7b95022eacc49ad5049
NathanaelGastelum <nathanaelgastelum@gmail.com>
2020-09-10 20:37:58 -0700

    Problem 1, AirPlane class

---
+++ PA0/Problem1/src/AirPlane.java
@@ -0,0 +1,54 @@
+public class AirPlane {
+    private String make;
+    private String model;
+    private double topSpeed;
+    private double range;
+
+    public AirPlane(String make, String model, double topSpeed, double range) {
+        this.make = make;
+        this.model = model;
+        this.topSpeed = topSpeed;
+        this.range = range;
+    }
+
+    public AirPlane() {
+        new AirPlane(
+                "Boeing",
+                "C-17",
+                590,
+                6456
+        );
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getTopSpeed() {
+        return topSpeed;
+    }
+
+    public void setTopSpeed(double topSpeed) {
+        this.topSpeed = topSpeed;
+    }
+
+    public double getRange() {
+        return range;
+    }
+
+    public void setRange(double range) {
+        this.range = range;
+    }
+}
