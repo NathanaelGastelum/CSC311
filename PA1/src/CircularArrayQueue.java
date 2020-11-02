5721b8413275f9e188888ba0cc323a62573bb1c4
NathanaelGastelum <nathanaelgastelum@gmail.com>
2020-11-01 22:47:32 -0800

    DoublyLinkedList implementation

---
+++ PA1/src/CircularArrayQueue.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+
+
+/**
+ * @author Ash J
+ * @param <E>
+ *
+ */
+
+public class CircularArrayQueue<E> implements Queue<E> {
+
+	public CircularArrayQueue(int queueSize) {
+		// TODO: create a new queue with the specified size
+	}
+	
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void enqueue(E e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public E first() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E dequeue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+    
+}
---
+++ PA1/src/Main.java
@@ -0,0 +1,17 @@
+
+
+public class Main{
+    /**
+     * @param args
+     * */
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+
+
+
+}
