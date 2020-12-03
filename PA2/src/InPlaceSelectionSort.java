b965aa4c590a8cdb905f5b085aa32e7cb4625d8b
NathanaelGastelum <nathanaelgastelum@gmail.com>
2020-12-02 20:51:43 -0800

    Selection sort

--- PA2/src/InPlaceSelectionSort.java
+++ PA2/src/InPlaceSelectionSort.java
@@ -17,7 +17,24 @@ public class InPlaceSelectionSort<K extends Comparable<K>> implements Sorter<K> 
 	 */
 	
 	public void sort(K[] array) {
-		// TODO Auto-generated method stub
+		// Loop over array
+		for (int i = 0; i < array.length - 1; i++) {
+
+			// loop over unsorted array
+			int minIndex = i;
+			for (int j = i + 1; j < array.length; j++) {
+				// log smallest value
+				if (array[minIndex].compareTo(array[j]) > 0) {
+					minIndex = j;
+				}
+				// swap min with beginning of unsorted array
+				if (minIndex != i){
+					K temp = array[minIndex];
+					array[minIndex] = array[i];
+					array[i] = temp;
+				}
+			}
+		}
 	}
 
 }
--- PA2/src/Main.java
+++ PA2/src/Main.java
@@ -3,13 +3,25 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
+        // Insertion Sort Test
         InPlaceInsertionSort<Integer> insertionSort = new InPlaceInsertionSort<>();
         Integer[] a1 = new Integer[]{5, 7, 8, 4, 9, 3, 2};
         Integer[] a2 = new Integer[]{2, 3, 4, 5, 7, 8, 9};
         insertionSort.sort(a1);
 
             assert Arrays.equals(a1, a2) : System.out.printf("%9s %s \n%9s %s \n","Expected:", Arrays.toString(a2), "Result:", Arrays.toString(a1));
-	
 
+        // Selection Sort Test
+        InPlaceSelectionSort<Integer> selectionsort = new InPlaceSelectionSort<>();
+        a1 = new Integer[]{5, 7, 8, 4, 9, 3, 2};
+        selectionsort.sort(a1);
+
+        assert Arrays.equals(a1, a2) : System.out.printf("%9s %s \n%9s %s \n","Expected:", Arrays.toString(a2), "Result:", Arrays.toString(a1));
+
+        a1 = new Integer[]{5, 7, 8, 4, 5, 3, 2};
+        a2 = new Integer[]{2, 3, 4, 5, 5, 7, 8};
+        selectionsort.sort(a1);
+
+        assert Arrays.equals(a1, a2) : System.out.printf("%9s %s \n%9s %s \n","Expected:", Arrays.toString(a2), "Result:", Arrays.toString(a1));
     }
 }
