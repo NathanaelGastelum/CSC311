bea8829c14bc8215ae58b66b415ee6a51ba4b181
NathanaelGastelum <nathanaelgastelum@gmail.com>
2020-12-02 22:26:56 -0800

    Heap sort

--- PA2/src/InPlaceHeapSort.java
+++ PA2/src/InPlaceHeapSort.java
@@ -16,7 +16,49 @@ public class InPlaceHeapSort<K extends Comparable<K>> implements Sorter<K> {
 	 * @param array - Array to sort
 	 */
 	public void sort(K[] array) {
-		// TODO Auto-generated method stub
+
+		buildMaxHeap(array);
+
+		int n = array.length;
+		for (int i = n - 1; i >= 0; i--) {
+			// Swap ends of unsorted array
+			K temp = array[i];
+			array[i] = array[0];
+			array[0] = temp;
+
+			n = n-1;
+			heapify(array, i,0);
+		}
 	}
 
+	private void buildMaxHeap(K[] array) {
+		int n = array.length;
+		for (int i = n/2 - 1; i >= 0; i--) {
+			heapify(array, n, i);
+		}
+	}
+
+	private void heapify(K[] array, int size, int i) {
+		int left = 2 * i + 1;
+		int right = 2 * i + 2;
+		int max = i;
+
+		// if left child is larger than root
+		if (left < size && array[left].compareTo(array[max]) > 0)
+			max = left;
+
+		// if right child is larger than max
+		if (right < size && array[right].compareTo(array[max]) > 0)
+			max = right;
+
+		// if max is not root
+		if (max != i) {
+			//swap array[i], array[max]
+			K temp = array[max];
+			array[max] = array[i];
+			array[i] = temp;
+
+			heapify(array, size, max);
+		}
+	}
 }
--- PA2/src/Main.java
+++ PA2/src/Main.java
@@ -8,20 +8,24 @@ public class Main {
         Integer[] a1 = new Integer[]{5, 7, 8, 4, 9, 3, 2};
         Integer[] a2 = new Integer[]{2, 3, 4, 5, 7, 8, 9};
         insertionSort.sort(a1);
-
-            assert Arrays.equals(a1, a2) : System.out.printf("%9s %s \n%9s %s \n","Expected:", Arrays.toString(a2), "Result:", Arrays.toString(a1));
+         assert Arrays.equals(a1, a2) : System.out.printf("%9s %s \n%9s %s \n","Expected:", Arrays.toString(a2), "Result:", Arrays.toString(a1));
 
         // Selection Sort Test
-        InPlaceSelectionSort<Integer> selectionsort = new InPlaceSelectionSort<>();
+        InPlaceSelectionSort<Integer> selectionSort = new InPlaceSelectionSort<>();
         a1 = new Integer[]{5, 7, 8, 4, 9, 3, 2};
-        selectionsort.sort(a1);
-
+        selectionSort.sort(a1);
         assert Arrays.equals(a1, a2) : System.out.printf("%9s %s \n%9s %s \n","Expected:", Arrays.toString(a2), "Result:", Arrays.toString(a1));
 
         a1 = new Integer[]{5, 7, 8, 4, 5, 3, 2};
         a2 = new Integer[]{2, 3, 4, 5, 5, 7, 8};
-        selectionsort.sort(a1);
+        selectionSort.sort(a1);
+        assert Arrays.equals(a1, a2) : System.out.printf("%9s %s \n%9s %s \n","Expected:", Arrays.toString(a2), "Result:", Arrays.toString(a1));
 
+        // Heap Sort Test
+        InPlaceHeapSort<Integer> heapSort = new InPlaceHeapSort<>();
+        a1 = new Integer[]{5, 7, 8, 4, 9, 3, 2};
+        a2 = new Integer[]{2, 3, 4, 5, 7, 8, 9};
+        heapSort.sort(a1);
         assert Arrays.equals(a1, a2) : System.out.printf("%9s %s \n%9s %s \n","Expected:", Arrays.toString(a2), "Result:", Arrays.toString(a1));
     }
 }
